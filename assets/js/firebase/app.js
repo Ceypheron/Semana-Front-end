a0c54fc58b397ea7f006b8fca2dbc2d20b24096d
Ceypheron <ceypheron@gmail.com>
2022-08-24 20:39:20 -0300

    s

---
+++ assets/js/firebase/app.js
@@ -0,0 +1,19 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCxy6CSfyY8QuvtQHUYPjL4dLxK14A0XIc",
+    authDomain: "semana-front-end.firebaseapp.com",
+    projectId: "semana-front-end",
+    storageBucket: "semana-front-end.appspot.com",
+    messagingSenderId: "427206595294",
+    appId: "1:427206595294:web:56365f68be9a06f6e7e84f"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  export default app
